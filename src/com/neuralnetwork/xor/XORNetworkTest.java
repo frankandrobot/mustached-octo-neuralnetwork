37e111cba33bf3623c0eeb1b56a09fdc8733a55d
uriel avalos <uri@frankandrobot.com>
2013-11-17 21:39:58 -0500

    [testing]

---
+++ src/com/neuralnetwork/xor/XORNetworkTest.java
@@ -0,0 +1,55 @@
+package com.neuralnetwork.xor;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class XORNetworkTest {
+
+    @org.junit.Test
+    public void testOutput() throws Exception
+    {
+        XORNetwork network = new XORNetwork();
+        NVector output;
+
+        output = network.output(new NVector(0f, 0f));
+
+        assertThat(output.size(), is(1));
+        assertThat(output.first(), is(0f));
+    }
+
+    @org.junit.Test
+    public void testOutput2() throws Exception
+    {
+        XORNetwork network = new XORNetwork();
+        NVector output;
+
+        output = network.output(new NVector(0f, 1f));
+
+        assertThat(output.size(), is(1));
+        assertThat(output.first(), is(1f));
+    }
+
+    @org.junit.Test
+    public void testOutput3() throws Exception
+    {
+        XORNetwork network = new XORNetwork();
+        NVector output;
+
+        output = network.output(new NVector(1f, 1f));
+
+        assertThat(output.size(), is(1));
+        assertThat(output.first(), is(0f));
+    }
+
+    @org.junit.Test
+    public void testOutput4() throws Exception
+    {
+        XORNetwork network = new XORNetwork();
+        NVector output;
+
+        output = network.output(new NVector(1f, 0f));
+
+        assertThat(output.size(), is(1));
+        assertThat(output.first(), is(1f));
+    }
+}
