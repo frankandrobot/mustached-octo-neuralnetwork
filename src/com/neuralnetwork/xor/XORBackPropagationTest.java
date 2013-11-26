f9fe3f7ff0f097202606732fb5f38d1a3597cace
uriel.avalos <uriel.avalos@teachhealth.com>
2013-11-26 16:14:12 -0500

    * refactored XOR network test

--- src/com/neuralnetwork/xor/XORBackPropagationTest.java
+++ src/com/neuralnetwork/xor/XORBackPropagationTest.java
@@ -7,10 +7,15 @@ public class XORBackPropagationTest
     @org.junit.Test
     public void testAllErrorsDecrease() throws Exception
     {
-        network.backpropagation(0.0001,
-                new NVector(0, 0), new NVector(0),
-                new NVector(0, 1), new NVector(1),
-                new NVector(1, 1), new NVector(0),
-                new NVector(1, 0), new NVector(1));
+        double tolerance = 0.0001;
+        network.backpropagation(tolerance, new NVector(0, 0), new NVector(0.2));
+        network.backpropagation(tolerance, new NVector(1, 0), new NVector(0.8));
+        network.backpropagation(tolerance, new NVector(1, 1), new NVector(0.2));
+        network.backpropagation(tolerance, new NVector(0, 1), new NVector(0.8));
+
+        System.out.println(network.output(new NVector(0, 0)));
+        System.out.println(network.output(new NVector(0, 1)));
+        System.out.println(network.output(new NVector(1, 1)));
+        System.out.println(network.output(new NVector(1, 0)));
     }
 }
