45a5dc7c71ad1870b79ba1ae9e461f282fe24686
uriel.avalos <uriel.avalos@teachhealth.com>
2013-12-10 15:59:41 -0500

    [testing]

--- test/com/neuralnetwork/convolutional/FeatureMapTest.java
+++ test/com/neuralnetwork/convolutional/FeatureMapTest.java
@@ -4,6 +4,9 @@ import com.neuralnetwork.core.ActivationFunctions;
 import com.neuralnetwork.core.Neuron;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class FeatureMapTest
 {
     @Test
@@ -18,7 +21,8 @@ public class FeatureMapTest
         final double[] weights = {0.1, 0.2, 0.3, 0.4, 0.5};
 
         FeatureMap.Builder builder = new FeatureMap.Builder();
-        Neuron neuron = new Neuron(new ActivationFunctions.SigmoidUnityFunction(), weights);
+        ActivationFunctions.SigmoidUnityFunction phi = new ActivationFunctions.SigmoidUnityFunction();
+        Neuron neuron = new Neuron(phi, weights);
         builder.setSharedNeuron(neuron);
         builder.setInputSize(3);
         builder.setReceptiveFieldSize(2);
@@ -26,5 +30,9 @@ public class FeatureMapTest
 
         featureMap.output(input);
 
+        double o11 = input[0][0]*weights[0] + input[0][1]*weights[1] + input[1][0]*weights[2] + input[1][1]*weights[3] + weights[4];
+        o11 = phi.apply(o11);
+
+        assertThat(featureMap.aFeatureMap[0][0], is(o11));
     }
 }
